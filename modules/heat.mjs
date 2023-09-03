1f04aa554d801e8bab54958da56efc4cbc49c2ee
Scott Henderson <scott.mc.henderson@gmail.com>
2023-09-02 22:31:08 -0700

    intial files

---
+++ modules/heat.mjs
@@ -0,0 +1,28 @@
+/* 
+
+GOAL : Given a certain level of heat resource & production, electrical resource & prodction, 
+and which generation you are in, can you reach max level tempearture? 
+
+Future TODOS:
+- Which generation will you reach max temperature? (When can you shift your focus?)
+- Depending on your income, can you supplement using Standard Projects (i.e. buying an Asteroid to raise temperature?)
+
+# Given a certain heat_resource, production, generation, will you reach max temperature by final generation?
+
+*/
+
+// Constsants for game
+const maxTemp = 8
+const startTemp = -30
+let maxGeneration = 12; // Can be 12 or 14 depending on game type
+
+let heatRes = 0; // Heat Resource
+let heatProd = 0; // Head Production
+let elecRes = 0; // Electrical Resource
+let elecProd = 0; // Electrical Production
+
+
+export function HeatCaclcuator(){
+    /* */
+
+}
